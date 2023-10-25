7b55b96d9e85e5ba1626a40527d2a607ef07925e
Dhananjai Bhalaik <Dbhalaik17@gmail.com>
2023-10-25 17:42:43 +0530

    crio

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -7,20 +7,37 @@ import java.net.MalformedURLException;
 
 public class App {
     public void getGreeting() throws InterruptedException, MalformedURLException {
-        
-        // This is to remove unnecessary warnings from your console
-        System.setProperty("java.util.logging.config.file", "logging.properties");
-        
-        TestCases tests = new TestCases(); // Initialize your test class
-
-        //TODO: call your test case functions one after other here
-
-        tests.testCase01();
-
-        //END Tests
+        //    Search_Amazon tests = new Search_Amazon(); // Initialize your test class
+        //     tests.browserlaunch();
+        //     tests.Amazon();
+        //     tests.endTest();
 
 
-        tests.endTest(); // End your test by clearning connections and closing browser
+        // Count_hyperlinks ch = new Count_hyperlinks();
+        // ch.browserlaunch();
+        // ch.Count_hyperlinks();
+        // ch.endTest();
+
+        Post_on_linkedin pl=new Post_on_linkedin();
+        pl.browserlaunch();
+        pl.Post_on_linkedin();
+        pl.endTest();
+
+        // Imdb_ratings rating = new Imdb_ratings();
+        // rating.browserlaunch();
+        // rating.Imdb_ratings();
+        // rating.endTest();
+
+        //  Window_handle hd = new Window_handle();
+        //  hd.browserlaunch();
+        //  hd.Window_handle();
+        //  hd.endTest();
+
+        // Frames_text ft = new Frames_text();
+        // ft.browserlaunch();
+        // ft.Frames_text();
+        // ft.endTest();
+
     }
 
     public static void main(String[] args) throws InterruptedException, MalformedURLException {
--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -3,6 +3,13 @@ package demo;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+//Selenium Imports
+import org.openqa.selenium.Keys;
+//import org.openqa.selenium.remote.BrowserType;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Select;
+///
 
 
 public class TestCases {
@@ -21,11 +28,11 @@ public class TestCases {
     {
         System.out.println("End Test: TestCases");
         driver.close();
-        driver.quit();
+        // driver.quit();
 
     }
 
-    
+
     public  void testCase01(){
         System.out.println("Start Test case: testCase01");
         driver.get("https://www.google.com");
